0c4c2c82ebdd1603af3c5ba49cf2a172a118ec79
majidbenmerah <99648395+majidbenmerah@users.noreply.github.com>
2023-04-20 13:18:23 +0200

    First deployment

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@ git init
 
 git add -A
 
-git commit -m "Deploiment"
+git commit -m "First deployment"
 
-git push -f https://github.com/majidbenmerah/todolist-express.js.git main
+git push -f https://github.com/majidbenmerah/todolist-express.js.git master
+
